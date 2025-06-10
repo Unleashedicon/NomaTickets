9b8e88136e608cd354386366d2e05847cfdd129a
unknown <kkipkurui123@gmail.com>
2025-06-10 19:41:33 +0300

    Fix profile user role error

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-    turbopack: false,
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
   },
-    reactStrictMode: true,
-      webpack: (config) => {
-        config.resolve.fallback = { fs: false, net: false, tls: false };
-        config.externals.push("pino-pretty", "lokijs", "encoding");
-        return config;
-      },
-    };
-    
-    module.exports = nextConfig;
+};
+
+module.exports = nextConfig;
