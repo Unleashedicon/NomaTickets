dd871362eafcb06a0c28028930b5832ed85baf86
unknown <kkipkurui123@gmail.com>
2025-06-11 02:42:14 +0300

    Fix profile user role error

---
+++ types/next-auth.d.ts
@@ -0,0 +1,24 @@
+// types/next-auth.d.ts
+import { DefaultSession } from "next-auth";
+import { JWT as DefaultJWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      role: "ADMIN" | "CREATOR" | "VIEWER";
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    id: string;
+    role: "ADMIN" | "CREATOR" | "VIEWER";
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    id: string;
+    role: "ADMIN" | "CREATOR" | "VIEWER";
+  }
+}
